43435b12085ad4abab27d71e0dcb637b6dbe820e
izeiseice <ice_5891@hotmail.com>
2017-09-01 02:22:39 +0700

    ya

--- HW2/main.bundle.js
+++ HW2/main.bundle.js
@@ -1,25 +1,10 @@
 "use strict";
 
-function input(e) {
-    var tbInput = document.getElementById("tbInput”);
-    tbInput.value = tbInput.value + e.value;
+function num(e) {
+    document.getElementById("bobo").innerHTML = document.getElementById("bobo").innerHTML + e;
 }
- 
-function del() {
-    var tbInput = document.getElementById("tbInput");
-    tbInput.value = tbInput.value.substr(0, tbInput.value.length - 1);
-}
-function load() {
-    var array = new Array();
- 
-    while (array.length < 10) {
-        var temp = Math.round(Math.random() * 9);
-        if (!contain(array, temp)) {
-            array.push(temp);
-        }
-    }
-    for (i = 0; i < 10; i++) {
-        var btn = document.getElementById("btn" + i);
-        btn.value = array[i];
-    }
+function bksp() {
+    var y = document.getElementById("bobo").innerHTML;
+    var x = document.getElementById("bobo").innerHTML.length;
+    document.getElementById("bobo").innerHTML = y.substring(0, x - 1);
 }

5c55a1c061d4833c14450a96b2f04d36ebe98f26
izeiseice <ice_5891@hotmail.com>
2017-09-01 02:47:45 +0700

    last la

--- HW2/main.bundle.js
+++ HW2/main.bundle.js
@@ -1,10 +1,10 @@
 "use strict";
 
 function num(e) {
-    document.getElementById("bobo").innerHTML = document.getElementById("bobo").innerHTML + e + " ";
+    document.getElementById("bobo").innerHTML=" "+document.getElementById("bobo").innerHTML+e;
 }
 function bksp() {
-    var y = document.getElementById("bobo").innerHTML;
-    var x = document.getElementById("bobo").innerHTML.length;
-    document.getElementById("bobo").innerHTML = y.substring(0, x - 2);
+    var y=document.getElementById("bobo").innerHTML;
+    var x=document.getElementById("bobo").innerHTML.length;
+    document.getElementById("bobo").innerHTML=y.substring(0,x-2);
 }
